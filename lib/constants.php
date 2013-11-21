f6b48d55b22c8a70ae2a300c9dd501c1d925f4f4
jcpeden <jcpeden@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-21 12:03:50 +0000

    Tagging version 1.0.9
    git-svn-id: https://plugins.svn.wordpress.org/easy-webtrends/tags/1.0.9@808198 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/constants.php
+++ lib/constants.php
@@ -5,17 +5,20 @@
  * @package EasyWebtrends
  * 
  * @author jcpeden
- * @version 1.0.6
+ * @version 1.0.9
  * @since 1.0.0
  */
 
 // The current version of this plugin
-if( !defined( 'EASYWEBTRENDS_VERSION' ) ) define( 'EASYWEBTRENDS_VERSION', '1.0.6' );
+if( !defined( 'EASYWEBTRENDS_VERSION' ) ) define( 'EASYWEBTRENDS_VERSION', '1.0.9' );
 
 // The directory the plugin resides in
-if( !defined( 'EASYWEBTRENDS_DIRNAME' ) ) define( 'EASYWEBTRENDS_DIRNAME', dirname( dirname( __FILE__ ) ) );
+if( !defined( 'EASYWEBTRENDS_PATH' ) ) define( 'EASYWEBTRENDS_PATH', dirname( dirname( __FILE__ ) ) );
+
+// The directory name of the plugin
+if( !defined( 'EASYWEBTRENDS_DIRNAME' ) ) define( 'EASYWEBTRENDS_DIRNAME', basename( EASYWEBTRENDS_PATH ) );
 
 // The URL path of this plugin
-if( !defined( 'EASYWEBTRENDS_URLPATH' ) ) define( 'EASYWEBTRENDS_URLPATH', WP_PLUGIN_URL . "/" . plugin_basename( EASYWEBTRENDS_DIRNAME ) );
+if( !defined( 'EASYWEBTRENDS_URLPATH' ) ) define( 'EASYWEBTRENDS_URLPATH', plugins_url( EASYWEBTRENDS_DIRNAME ) );
 
 if( !defined( 'IS_AJAX_REQUEST' ) ) define( 'IS_AJAX_REQUEST', ( !empty( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && strtolower( $_SERVER['HTTP_X_REQUESTED_WITH'] ) == 'xmlhttprequest' ) );
